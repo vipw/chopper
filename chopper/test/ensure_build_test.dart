7b29d68131d2ccbc8336a1daac43fa2fc8c68018
Hadrien Lejard <hadrien@lefty.io>
2021-04-10 14:05:26 +0200

    comments

--- chopper/test/ensure_build_test.dart
+++ chopper/test/ensure_build_test.dart
@@ -4,6 +4,7 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
+  // check that pub run build_runner build does not produce changes
   test(
     'ensure_build',
     () => expectBuildClean(packageRelativeDirectory: 'chopper'),
